02477fc6b411eaea4d58df7418e9d92a15f8fada
1maximsafronov <1maximsafronov@gmail.com>
2019-09-11 04:46:01 +0700

    Добавил скрипт для открытия модальных окон

--- js/script.js
+++ js/script.js
@@ -3,6 +3,12 @@ var slider = document.querySelector('.promo__slider');
 var slides = slider.querySelectorAll('.slider__item');
 var slidesDots = slider.querySelectorAll('.slider__dot');
 
+var modals = document.querySelectorAll('.modal');
+var mapModal  = document.querySelector('.modal-map');
+var writeUsModal = document.querySelector('.modal-write-us');
+var mapLink = document.querySelector('.contacts__map-link');
+var writeUsLink = document.querySelector('.contacts__write-us');
+
 var slideIndex = 0;
 var slideActive = slides[slideIndex];
 var slideDotActive = slidesDots[slideIndex];
@@ -20,7 +26,7 @@ function changeSlide(itemIndex){
   slideDotActive = slidesDots[itemIndex];
 };
 
-// Обработчик клика
+// Обработчик клика на слайдер
 slider.addEventListener("click", function(event){
   var indexTmp = slideIndex;
   var targetClick = event.target;
@@ -54,4 +60,40 @@ slider.addEventListener("click", function(event){
     changeSlide(slideIndex);
   }
 });
+
+
+// Открытие блока с картой
+mapLink.addEventListener("click", function(event){
+    event.preventDefault();
+    modalMap.classList.add("modal--show");
+});
+// Открытие окна с формой обратной связи
+writeUsLink.addEventListener("click", function(event){
+  event.preventDefault();
+  modalWriteUs.classList.add("modal--show");
+});
+// Функция закрытия модальных блоков по клику
+function closeModal(modalElement){
+  var modalCloseBtn = modalElement.querySelector('.modal__close');
+  modalElement.addEventListener("click", function(event){
+    if(event.target == modalCloseBtn){
+      event.preventDefault();
+      console.log("works");
+      modalElement.classList.remove('modal--show');
+    }
+  });
+}
+for(var i = 0; i < modals.length; i++){
+  closeModal(modals[i]);
+}
+// Закрытие всех модалок по esc
+document.addEventListener('keypress', function (event) {
+  if(event.keyCode === 27){
+    event.preventDefault();
+    for(var i = 0; i < modals.length; i++){
+      modals[i].classList.remove('modal--show');
+    }
+  }
+});
+
 */
